0be6120288e49562020d78414e23be35816136b3
posampas <piotr.mierzejewski@mail.com>
2023-06-09 14:24:44 +0200

    Create pipeline for ingestion

---
+++ src/czaspracy/pipelines/ingest_raw_logs/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'ingest_raw_logs'
+generated using Kedro 0.18.9
+"""
---
+++ src/czaspracy/pipelines/ingest_raw_logs/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'ingest_raw_logs'
+generated using Kedro 0.18.9
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
